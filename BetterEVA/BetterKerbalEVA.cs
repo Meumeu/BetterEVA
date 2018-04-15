c6aff28d9c8dc7264d2b30af065fcfdc4721fabd
Guillaume Meunier <guillaume.meunier@centraliens.net>
2018-04-15 19:06:50 +0200

    press F to cancel relative velocity

--- BetterEVA/BetterKerbalEVA.cs
+++ BetterEVA/BetterKerbalEVA.cs
@@ -5,7 +5,6 @@ using System.Reflection;
 // TODO: limit camera rotation speed in space
 // TODO: control yaw with mouse on ground
 // TODO: command to face the target
-// TODO: command to cancel relative velocity
 
 namespace BetterEVA
 {
@@ -23,11 +22,17 @@ namespace BetterEVA
 
         static readonly FieldInfo eva_tgtFwd = typeof(KerbalEVA).GetField("tgtFwd", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
         static readonly FieldInfo eva_tgtUp = typeof(KerbalEVA).GetField("tgtUp", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        static readonly FieldInfo eva_packTgtRPos = typeof(KerbalEVA).GetField("packTgtRPos", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+        [KSPField]
+        public float MinPitch = -1.0f;
+
+        [KSPField]
+        public float MaxPitch = 1.5f;
+
+        [KSPField]
+        public float translation_Kp = 1;
         
-        const float MinPitch = -1.0f;
-        const float MaxPitch = 1.5f;
-
-
         [KSPField(guiName = "First person camera", guiActive = true, isPersistant = true), UI_Toggle(disabledText = "Disabled", enabledText = "Enabled")]
         public bool FirstPerson = false;
 
@@ -95,10 +100,29 @@ namespace BetterEVA
             }
         }
 
+        void TranslationControl()
+        {
+            if (Input.GetKey(KeyCode.F) && vessel.targetObject != null)
+            {
+                Vector3 packTgtRPos = (Vector3)eva_packTgtRPos.GetValue(eva);
+
+                Vector3 vel_error = vessel.GetObtVelocity() - vessel.targetObject.GetObtVelocity();
+                packTgtRPos = Vector3.ClampMagnitude(packTgtRPos - translation_Kp * vel_error, 1.0f);
+                
+                eva_packTgtRPos.SetValue(eva, packTgtRPos);
+            }
+        }
+
         void FixedUpdate()
         {
             if (eva == null)
+            {
                 eva = part.FindModuleImplementing<KerbalEVA>();
+
+                // add the callback before the others to have a chance to modify the packTgtRPos member
+                eva.st_idle_fl.OnFixedUpdate = this.TranslationControl + eva.st_idle_fl.OnFixedUpdate;
+                eva.st_idle_gr.OnFixedUpdate = this.TranslationControl + eva.st_idle_gr.OnFixedUpdate;
+            }
 
             switch (mode)
             {
